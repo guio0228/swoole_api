198ddedbc3d9aad1cc472b0c89abfc8b2acd6873
guio0228 <93069491+guio0228@users.noreply.github.com>
2024-09-04 19:24:49 +0800

    update

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -3,7 +3,7 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f ".env"]; then
+if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
     cp .env.example .env
 else
